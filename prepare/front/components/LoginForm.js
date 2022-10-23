b75fe062556e49992e7c535e3612f3c6e22af60c
kimsungkwang <skk405@naver.com>
2022-10-23 20:12:32 +0900

    리덕스 구현

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import styled from "styled-components";
 import Proptypes from "prop-types";
 import useInput from "../hooks/useInput";
+import { loginAction } from "../reducers";
+import { useDispatch } from "react-redux";
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -13,14 +15,14 @@ const FormWrapper = styled(Form)`
 padding 10px;
 `;
 
-const LoginForm = ({ setIsLoggedIn }) => {
-
-  const [id, onChangeId] = useInput('');
-  const [password, onChangePassword] = useInput('');
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const [id, onChangeId] = useInput("");
+  const [password, onChangePassword] = useInput("");
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
-    setIsLoggedIn(true);
+    dispatch(loginAction(id, password));
   }, [id, password]);
 
   return (
@@ -47,10 +49,6 @@ const LoginForm = ({ setIsLoggedIn }) => {
       </ButtonWrapper>
     </FormWrapper>
   );
-};
-
-LoginForm.prototype = {
-  setIsLoggedIn: Proptypes.func.isRequired,
 };
 
 export default LoginForm;
