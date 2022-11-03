b14fbaf90be30d158b6bdae3710b8ac7703d6a96
kimsungkwang <skk405@naver.com>
2022-11-03 23:50:10 +0900

    라우터 분리

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const postRouter = require("./routes/post")
 
 const app = express();
 
@@ -26,13 +27,7 @@ app.get("/api/posts", (req, res) => {
   ]);
 });
 
-app.post("/api/post", (req, res) => {
-  res.json({ id: 1, content: "hello" });
-});
-
-app.delete("/api/post", (req, res) => {
-  res.json({ id: 1 });
-});
+app.use("/post", postRouter);
 
 app.listen(3065, () => {
   console.log("서버 실행 중 ");
